d315289fc91cebe10e4ffe3d9d78eea414e9f93d
Rajput-Suraj <surajarajput@gmail.com>
2022-03-31 14:33:24 +0530

    Styles expense item component using css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,15 @@
-import React from "react";
+import "./ExpenseItem.css";
 
 const ExpenseItem = () => {
-    return <div>ExpenseItem</div>;
+    return (
+        <div className="expense-item">
+            <div>March 31st 2022</div>
+            <div className="expense-item__description">
+                <h2>Car Insurance</h2>
+                <div className="expense-item__price">$295.5</div>
+            </div>
+        </div>
+    );
 };
 
 export default ExpenseItem;
